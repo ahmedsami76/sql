049de973ce5f6c861b3841be57b8c8c307af99a1
Ahmed Sami <Ahmed.Sami@yahoo.com>
2022-06-07 20:25:31 -0500

    add command.sh file

---
+++ commands.sh
@@ -0,0 +1,28 @@
+####
+# Install MySQL
+sudo apt install mysql-server -y
+sudo systemctl start mysql
+
+# Connect to mysql interactive shell
+sudo mysql 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'P@ssw0rd';
+SOURCE /home/sami/relational-databases/databases/sakila-db/sakila-schema.sql;
+USE sakila;
+SOURCE /home/sami/relational-databases/databases/sakila-db/sakila-data.sql;
+SHOW TABLES FROM sakila;
+SELECT * FROM actor LIMIT 10;
+DESC actor;
+SHOW COLUMNS FROM actor;
+SHOW CREATE TABLES actor\G
+
+####
+# clone repo
+git config user.name "Ahmed Sami"
+git config user.email "ahmed.sami@yahoo.com"
+git clone https://github.com/ahmedsami76/relational-databases.git
+cd relational-databases
+
+# Use Docker
+docker run -d -v /var/run/docker.sock:/tmp/docker.sock -v /etc/hosts:/tmp/hosts asami76/docker-hoster
+
+docker-compose -f mysql up
