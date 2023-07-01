80e4a166b5c840f89468193ec4b065e56746c0ee
Ahmed Sami <ahmed.sami@yahoo.com>
2023-06-30 23:53:59 -0500

    Update 0.INIT.sql

--- databases/sql/0.INIT.sql
+++ databases/sql/0.INIT.sql
@@ -1,4 +1,6 @@
-SOURCE /home/sami/relational-databases/databases/sakila-db/sakila-schema.sql;
+-- make sure you have the mysql.yml compose up and running and you execute the below commands from mysql-workbench or the vscode database client extention
+SOURCE /usr/databases/sakila-db/sakila-schema.sql;
 USE sakila;
-SOURCE /home/sami/relational-databases/databases/sakila-db/sakila-data.sql;
+SOURCE /usr/databases/sakila-db/sakila-data.sql;
+
 SHOW TABLES FROM sakila;
