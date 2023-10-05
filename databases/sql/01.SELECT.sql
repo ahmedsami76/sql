94b735f2aae44c2803bff23855950f0abdf1f793
Hazem Omar <hazematwwa@gmail.com>
2023-10-05 02:26:47 -0700

    added in 5/10

--- databases/sql/01.SELECT.sql
+++ databases/sql/01.SELECT.sql
@@ -29,7 +29,7 @@ SELECT "'This is a quoted text'"; -- embed different qoutes
 SELECT "\'This is a quoted text\'";
 SELECT "This is a single quote \' and this is a doublequote \"";
 SELECT "Line 1\nLine 2\nLine 3";
-SELECT "This is a path in windows C:\\my\\folder\\path";
+SELECT "This is a path in windows C:\\my\\folder\\path"";
 
 -- arithmetic
 SELECT 1 + 2;
